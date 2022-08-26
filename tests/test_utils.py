6512e01f7f52250ca44e6f287d86db4d7bb564fa
Valentin Rigal <rigal@teklia.com>
2022-08-26 10:59:28 +0200

    Add tests for Redis message queue recovery

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import os
+import pickle
+import signal
+from unittest.mock import Mock
+
+import pytest
 
 from libmozevent import utils
+from libmozevent.bus import AsyncRedis
+from libmozevent.bus import MessageBus
+from libmozevent.bus import RedisQueue
+
+
+def unexpected_error():
+    raise Exception("Unexpected")
 
 
 def test_run_tasks():
@@ -20,3 +33,65 @@ def test_run_tasks():
 
     utils.run_tasks(set([do(), do(), do()]))
     assert count == 9
+
+
+@pytest.mark.parametrize(
+    "task_side_effect",
+    [
+        lambda: os.kill(os.getpid(), signal.SIGTERM),
+        unexpected_error,
+    ],
+)
+def test_run_tasks_restore_redis_messages(task_side_effect):
+    """
+    When using a Redis queue, messages are restored in case of an unexpected failure or
+    if a SIGTERM is received
+    """
+    redis_mock = Mock(spec=RedisQueue)
+
+    async def redis_connect():
+        return redis_mock
+
+    AsyncRedis.connect = Mock(side_effect=redis_connect)
+
+    redis_queue = []
+
+    async def await_rpush(*args):
+        redis_queue.append(args)
+
+    async def await_lpush(*args):
+        redis_queue.insert(0, args)
+
+    async def await_blpop(*args):
+        return redis_queue.pop(0)
+
+    redis_mock.rpush = await_rpush
+    redis_mock.lpush = await_lpush
+    redis_mock.blpop = await_blpop
+
+    os.environ.update(REDIS_URL="redis://localhost")
+    bus = MessageBus()
+
+    bus.add_queue("input", redis=True)
+    assert len(bus.queues) == 1
+    assert redis_queue == []
+
+    asyncio.get_event_loop().run_until_complete(bus.send("input", "message"))
+    queue_name = bus.queues["input"].name
+    message = pickle.dumps("message", protocol=pickle.HIGHEST_PROTOCOL)
+
+    assert redis_queue == [(queue_name, message)]
+
+    async def deadly_task():
+        await bus.receive("input")
+        # Ensure the message has been consumed
+        assert redis_queue == []
+        # Raise an unexpected error or send a TERM signal to itself
+        task_side_effect()
+        await asyncio.sleep(10)
+
+    with pytest.raises(asyncio.CancelledError):
+        utils.run_tasks([deadly_task()], bus=bus)
+
+    # Message has been send back in the queue
+    assert redis_queue == [(queue_name, message)]
